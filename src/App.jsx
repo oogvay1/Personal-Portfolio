6cd666fd0e38b1a0eb4aa7cae28ac5a07ff575a2
oogvay1 <atalgat592@gmail.com>
2025-06-21 13:11:40 +0500

    start Portfolio

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      {/* <spline-viewer className="spline" loading-anim-type="spinner-small-dark" url="https://prod.spline.design/3SOzmZWEUi4Pnz2M/scene.splinecode"></spline-viewer> */}
+    </>
+  );
+}
+
+export default App;
