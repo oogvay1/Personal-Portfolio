0cb2a69cc371c4fc7dbf1e3936dc4835829e95ed
oogvay1 <atalgat592@gmail.com>
2025-06-27 07:53:38 +0500

    Navigation bar

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -8,7 +8,11 @@ function Navigation() {
                 <div className="nav-text">
                     <h1>Home</h1>
                     <h1>About</h1>
+                    <h1>Work</h1>
+                    <h1>Contact</h1>
                 </div>
+
+                
 
                 {/* <spline-viewer className="spline" url="https://prod.spline.design/zcV76R-jCGhAadMc/scene.splinecode"></spline-viewer> */}
             </div>
