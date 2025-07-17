0e69a4ce2fd5996f57fa23b9b0061ef33d77f00a
oogvay1 <atalgat592@gmail.com>
2025-07-17 07:26:02 +0500

    ывап

--- src/Components/Works/Works.jsx
+++ src/Components/Works/Works.jsx
@@ -43,12 +43,24 @@ function Works() {
                         <div className="work-container">
                             <div className="screw-img-container">
                                 <Screw src="src/assets/Project1.png" />
+                                <div className="project-date">
+                                    <p>{projects[0].name}</p>
+                                    <p>{projects[0].date}</p>
+                                </div>
                             </div>
                             <div className="screw-l">
                                 <Screw src="src/assets/Project3.png" />
+                                <div className="project-date">
+                                    <p>{projects[3].name}</p>
+                                    <p>{projects[3].date}</p>
+                                </div>
                             </div>
-                            <div className="screw-img-container">
+                            <div style={{marginBottom: "100px"}} className="screw-img-container">
                                 <Screw src="src/assets/Project2.png" />
+                                <div className="project-date">
+                                    <p>{projects[2].name}</p>
+                                    <p>{projects[2].date}</p>
+                                </div>
                             </div>
 
                             {/* {projects.map((el, index) => (
@@ -65,12 +77,24 @@ function Works() {
                         <div className="work-container">
                             <div className="screw-img-container">
                                 <Screw src="src/assets/Project4.png" />
+                                <div className="project-date">
+                                    <p>{projects[4].name}</p>
+                                    <p>{projects[4].date}</p>
+                                </div>
                             </div>
                             <div className="screw-l">
                                 <Screw src="src/assets/Project7.png" />
+                                <div className="project-date">
+                                    <p>{projects[6].name}</p>
+                                    <p>{projects[6].date}</p>
+                                </div>
                             </div>
                             <div className="screw-img-container">
                                 <Screw src="src/assets/Project5.png" />
+                                <div className="project-date">
+                                    <p>{projects[5].name}</p>
+                                    <p>{projects[5].date}</p>
+                                </div>
                             </div>
                         </div>
                     </div>
