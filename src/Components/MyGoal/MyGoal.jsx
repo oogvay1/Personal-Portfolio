015baa3ac44d6745e76f145eda80d91bc15b864e
oogvay1 <atalgat592@gmail.com>
2025-07-21 14:49:34 +0500

    sdfg

--- src/Components/MyGoal/MyGoal.jsx
+++ src/Components/MyGoal/MyGoal.jsx
@@ -5,7 +5,10 @@ function MyGoal() {
     return (
         <>
             <h2>
-                
+                Built with intention.
+                I craft modern front-end experiences with precision and clarity —
+                where design speaks, interaction flows, and performance leads.
+                Freelance-ready. Vision-focused. Always evolving.
             </h2>
         </>
     );
