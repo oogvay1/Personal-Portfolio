2fb84e7da4f385e395fbb892284ded2183fb154f
oogvay1 <atalgat592@gmail.com>
2025-07-23 18:11:42 +0500

    asdf

--- src/Components/MyGoal/MyGoal.jsx
+++ src/Components/MyGoal/MyGoal.jsx
@@ -11,26 +11,24 @@ function MyGoal() {
 
     return (
         <>
-            <div className="my-goal-container">
-                <div className="my-goal">
-                    <motion.div
-                        className='mask'
-                        animate={{
-                            webkitMaskPosition: `${x - size / 2}px ${y - size / 2}px`,
-                            webkitMaskSize: `${size}px`
-                        }}
-                        transition={{ type: "tween", ease: "backOut", duration: .5 }}
-                    >
-                        <p>
-                            I build full-stack websites from scratch for agencies and individuals — if the effort matches the reward.
-                        </p>
-                    </motion.div>
-
-                    <p onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}
-                        className='main-text-mask'>
-                        Ꝅreative in frꙨnt-en₫ and back-en₫, ֎ usﭐng ͼutting-edꞡe teⴛ bꭎilt ꚍo l₳st ⇀ nꙨt to bɘ Ꞅewrꬸtten.
+            <div className="my-goal">
+                <motion.div
+                    className='mask'
+                    animate={{
+                        webkitMaskPosition: `${x - size / 2}px ${y - size / 2}px`,
+                        webkitMaskSize: `${size}px`
+                    }}
+                    transition={{ type: "tween", ease: "backOut", duration: .5 }}
+                >
+                    <p>
+                        I build full-stack websites from scratch for agencies and individuals — if the effort matches the reward.
                     </p>
-                </div>
+                </motion.div>
+
+                <p onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}
+                    className='main-text-mask'>
+                    Ꝅreative in frꙨnt-en₫ and back-en₫, ֎ usﭐng ͼutting-edꞡe teⴛ bꭎilt ꚍo l₳st ⇀ nꙨt to bɘ Ꞅewrꬸtten.
+                </p>
             </div>
         </>
     );
