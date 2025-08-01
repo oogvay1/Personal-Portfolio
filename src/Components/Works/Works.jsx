89f530a5a3008a9ed5f258c349321ef4f8e7d036
oogvay1 <atalgat592@gmail.com>
2025-08-01 11:26:34 +0500

    sdfg

--- src/Components/Works/Works.jsx
+++ src/Components/Works/Works.jsx
@@ -3,14 +3,26 @@ import CountUp from '../Counte/Couter';
 import Screw from '../Screw/Screw';
 import { projects } from '../ProjectList/ProjectList';
 import './Works.css';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { motion, useScroll, useTransform } from "framer-motion";
 
 const Works = forwardRef(function Works(props, ref) {
 
+    useEffect(() => {
+        ScrollTrigger.create({
+            trigger: ".works",
+            start: "top center",
+            duration: 2,
+            onEnter: () => document.querySelector('body').style.background = "#0b0b0b",
+            onLeaveBack: () => document.querySelector('body').style.background = "#F3F3F3",
+        })
+    }, []);
+
     const main = useRef(null);
     const { scrollYProgress } = useScroll({ target: main })
 
-    const x = useTransform(scrollYProgress, [0, 1], ["0%", "-55%"]);
+    const x = useTransform(scrollYProgress, [0, 1], ["0%", "-110%"]);
 
     return (
         <>
